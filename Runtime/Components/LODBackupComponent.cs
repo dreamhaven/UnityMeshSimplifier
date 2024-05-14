ddb7f33aa380607328016d7a9af8b048cdfa9fcb
Stephen Coan <103901155+scoan-sd@users.noreply.github.com>
2024-05-14 16:56:34 -0700

    expose LODBackupComponent
    expose LODBackupComponent

--- Runtime/Components/LODBackupComponent.cs
+++ Runtime/Components/LODBackupComponent.cs
@@ -29,7 +29,7 @@ using UnityEngine;
 namespace UnityMeshSimplifier
 {
     [AddComponentMenu("")]
-    internal class LODBackupComponent : MonoBehaviour
+    public class LODBackupComponent : MonoBehaviour
     {
         [SerializeField]
         private Renderer[] originalRenderers = null;
